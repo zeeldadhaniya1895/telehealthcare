c88c3fc57cbfba39c74d2d1123f6f4169d216244
Param Savjani <paramsavjani3010@gmail.com>
2024-09-28 11:21:42 +0530

    remove extra login buton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,6 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
   <>
-  <LoginButton asChild>
-    <Button variant="secondary" size="lg" className="self-center">
-        login
-    </Button>
-    </LoginButton>
   </>
   );
 }
